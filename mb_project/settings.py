70c2a34360c832f43c1fa510df79f69e539cc55c
Manuel <mrgiraldoo@gmail.com>
2020-08-22 16:51:34 -0500

    adding some changes

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'zrcq)g!snapd0(sgir#eg_8lqnq7gu@pq!2-9)x*016=!xs-(h'
+#DJANGO_SECRET_KEY = 'zrcq)g!snapd0(sgir#eg_8lqnq7gu@pq!2-9)x*016=!xs-(h'
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
